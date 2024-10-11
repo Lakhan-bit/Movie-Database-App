e88d7ae618cfd24110b8cfe550c58d2d654c2cd7
lakhan khobragade <lakhankhobragade2003@gmail.com>
2024-10-11 20:33:11 +0530

    new change

--- src/App.js
+++ src/App.js
@@ -5,7 +5,10 @@ import MovieSearch from './components/MovieSearch';
 function App() {
   return (
     <div className="App">
+      <p>this is movie reymi moviesearch</p>
+      <p>reymi</p>
       <MovieSearch/>
+      
     </div>
   );
 }
